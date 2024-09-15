6a143bb9594c8132bcbfbfb3daa0a8f4ee653566
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-15 11:21:06 -0400

    ✨ (breadboard-with-jacks.ts): add support for additional jacks ports in the design to enhance functionality of the breadboard model

--- src/breadboard-with-jacks.ts
+++ src/breadboard-with-jacks.ts
@@ -1,5 +1,6 @@
-import { cube, cuboid } from "@jscad/modeling/src/primitives";
+import { cube, cuboid, cylinder } from "@jscad/modeling/src/primitives";
 import { subtract, union } from "@jscad/modeling/src/operations/booleans";
+import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
 
 const breadboard = {
   width: 100,
@@ -18,23 +19,47 @@ const jacks = {
   clearance: 30,
 };
 
-const body = () => {
+const body = ({ width, height, length, padding }: typeof breadboard) => {
+  return subtract(
+    cuboid({
+      size: [width + padding.x * 2, height + padding.y * 2, length + padding.z],
+    }),
+    cuboid({
+      center: [0, 0, padding.z / 2],
+      size: [width, height, length],
+    })
+  );
+};
+
+const jackPorts = (
+  { diameter, shaft, clearance }: typeof jacks,
+  { width, height, length, padding }: typeof breadboard
+) => {
   return subtract(
     cuboid({
       size: [
-        breadboard.width + breadboard.padding.x * 2,
-        breadboard.height + breadboard.padding.y * 2,
-        breadboard.length + breadboard.padding.z,
+        width + padding.x * 2,
+        clearance + padding.y * 2,
+        clearance + padding.z,
       ],
     }),
-    cuboid({
-      center: [0, 0, breadboard.padding.z / 2],
-      size: [breadboard.width, breadboard.height, breadboard.length],
-    })
+    rotate(
+      [0, Math.PI / 2, 0],
+      union(
+        cuboid({
+          center: [-clearance / 2, 0, 0],
+          size: [clearance, clearance + padding.x * 2, width],
+        }),
+        cylinder({
+          radius: clearance / 2,
+          height: width + padding.x * 2 - shaft * 2,
+        })
+      )
+    )
   );
 };
 
 // A function declaration that returns geometry
 export const main = () => {
-  return body();
+  return jackPorts(jacks, breadboard);
 };

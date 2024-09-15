2755de98225c49a8421282ea7c59c8fb23c3c167
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-15 10:47:50 -0400

    ✨ (breadboard-with-jacks.ts): introduce a new file 'breadboard-with-jacks.ts' with functions to create a breadboard model with jacks.

---
+++ src/breadboard-with-jacks.ts
@@ -0,0 +1,36 @@
+import { cube, cuboid } from "@jscad/modeling/src/primitives";
+import { subtract, union } from "@jscad/modeling/src/operations/booleans";
+
+const breadboard = {
+  width: 100,
+  height: 50,
+  length: 10,
+  padding: {
+    x: 10,
+    y: 10,
+    z: 10,
+  },
+};
+
+const jacks = {
+  diameter: 20,
+  shaft: 10,
+  clearance: 30,
+};
+
+// A function declaration that returns geometry
+export const main = () => {
+  return subtract(
+    cuboid({
+      size: [
+        breadboard.width + breadboard.padding.x * 2,
+        breadboard.height + breadboard.padding.y * 2,
+        breadboard.length + breadboard.padding.z,
+      ],
+    }),
+    cuboid({
+      center: [0, 0, breadboard.padding.z / 2],
+      size: [breadboard.width, breadboard.height, breadboard.length],
+    })
+  );
+};

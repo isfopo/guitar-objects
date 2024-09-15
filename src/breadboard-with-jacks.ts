a8ae13509ef4e1c70cb24beca1db028ca9108fe4
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-15 16:30:36 -0400

    🐛 (breadboard-with-jacks.ts): adjust dimensions and padding values in breadboard and jacks objects for proper rendering and positioning of components

--- src/breadboard-with-jacks.ts
+++ src/breadboard-with-jacks.ts
@@ -8,29 +8,25 @@ import { subtract, union } from "@jscad/modeling/src/operations/booleans";
 import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
 
 const breadboard = {
-  width: 100,
-  height: 50,
+  width: 166,
+  height: 51,
   length: 10,
-  padding: {
-    x: 10,
-    y: 10,
-    z: 10,
-  },
+  padding: 5,
 };
 
 const jacks = {
-  diameter: 20,
-  shaft: 10,
+  diameter: 10,
+  shaft: 2,
   clearance: 30,
 };
 
 const body = ({ width, height, length, padding }: typeof breadboard) => {
   return subtract(
     cuboid({
-      size: [width + padding.x * 2, height + padding.y * 2, length + padding.z],
+      size: [width + padding * 2, height + padding * 2, length + padding],
     }),
     cuboid({
-      center: [0, 0, padding.z / 2],
+      center: [0, 0, padding / 2],
       size: [width, height, length],
     })
   );
@@ -38,30 +34,26 @@ const body = ({ width, height, length, padding }: typeof breadboard) => {
 
 const jackPorts = (
   { diameter, shaft, clearance }: typeof jacks,
-  { width, height, length, padding }: typeof breadboard
+  { width, padding }: typeof breadboard
 ) => {
   return subtract(
-    roundedCuboid({
-      size: [
-        width + padding.x * 2,
-        clearance + padding.y * 2,
-        clearance + padding.z,
-      ],
+    cuboid({
+      size: [width + padding * 2, clearance + padding * 2, clearance + padding],
     }),
     rotate(
       [0, Math.PI / 2, 0],
       union(
         cuboid({
-          center: [-clearance / 2, 0, 0],
-          size: [clearance, clearance + padding.x * 2, width],
+          center: [-(clearance + padding * 2) / 2, 0, 0],
+          size: [clearance + padding * 2, clearance + padding * 2, width],
         }),
         cylinder({
           radius: clearance / 2,
-          height: width + padding.x * 2 - shaft * 2,
+          height: width + padding * 2 - shaft * 2,
         }),
         cylinder({
           radius: diameter / 2,
-          height: width + padding.x * 2,
+          height: width + padding * 2,
         })
       )
     )
@@ -73,7 +65,11 @@ export const main = () => {
   return union(
     body(breadboard),
     translate(
-      [0, breadboard.height, (jacks.clearance - breadboard.padding.x) / 2],
+      [
+        0,
+        breadboard.height - breadboard.padding,
+        jacks.clearance / 2 - breadboard.padding,
+      ],
       jackPorts(jacks, breadboard)
     )
   );

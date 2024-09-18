d7d6ff0baaaa5ce9778cced55900cd97f3ce8f90
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-17 23:50:59 -0400

    🔧 (bass-tuner-ferrule.ts): added imports for triangle, rotate, translate, and degToRad functions   ♻️ (bass-tuner-ferrule.ts): refactored bottom function to improve readability and maintainability by splitting the geometry creation into smaller, more specialized functions.   🐛 (bass-tuner-ferrule.ts): fixed the radius value assigned to the splines object to 0.5 for correct geometry calculations.

--- src/bass-tuner-ferrule.ts
+++ src/bass-tuner-ferrule.ts
@@ -1,6 +1,8 @@
 import { subtract, union } from "@jscad/modeling/src/operations/booleans";
-import { circle, cylinder } from "@jscad/modeling/src/primitives";
+import { circle, cylinder, triangle } from "@jscad/modeling/src/primitives";
 import { extrudeRotate } from "@jscad/modeling/src/operations/extrusions";
+import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
+import { degToRad } from "@jscad/modeling/src/utils";
 
 const segments = 50;
 
@@ -15,7 +17,7 @@ const height = {
 };
 const splines = {
   count: 24,
-  radius: 1,
+  radius: 0.5,
   inset: 0.5,
 };
 
@@ -37,12 +39,43 @@ const top = () => {
 };
 
 const bottom = () => {
-  return cylinder({
-    height: height.bottom,
-    radius: (diameter.inner + splines.inset) / 2,
-    center: [0, 0, -height.bottom / 2],
-    segments,
-  });
+  const splinesGeo = [...Array(splines.count).keys()].map((i) =>
+    rotate(
+      [0, 0, i * ((2 * Math.PI) / splines.count)],
+      cylinder({
+        height: height.bottom,
+        radius: splines.radius,
+        segments: 4,
+        center: [(diameter.inner + splines.inset) / 2, 0, -height.bottom / 2],
+      })
+    )
+  );
+
+  const bevel = extrudeRotate(
+    { segments },
+    translate(
+      [diameter.shaft / 2, -height.bottom],
+      triangle({
+        type: "SAS",
+        values: [
+          diameter.inner - diameter.shaft,
+          degToRad(90),
+          diameter.inner - diameter.shaft,
+        ],
+      })
+    )
+  );
+
+  return subtract(
+    cylinder({
+      height: height.bottom,
+      radius: (diameter.inner + splines.inset) / 2,
+      center: [0, 0, -height.bottom / 2],
+      segments,
+    }),
+    ...splinesGeo,
+    bevel
+  );
 };
 
 const shaft = () =>

03836f3a35136ef30021e72efcc053a48ff4ccb8
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-17 22:06:26 -0400

    ♻️ (bass-tuner-ferrule.ts): refactor variable names to improve code clarity and maintainability   ✨ (bass-tuner-ferrule.ts): add new 'inset' property to the 'splines' object for more precise calculations

--- src/bass-tuner-ferrule.ts
+++ src/bass-tuner-ferrule.ts
@@ -3,20 +3,19 @@ import { cylinder } from "@jscad/modeling/src/primitives";
 
 const segments = 50;
 
-const ferrule = {
-  diameter: {
-    shaft: 8,
-    inner: 10,
-    outer: 12,
-  },
-  height: {
-    bottom: 10,
-    top: 4,
-  },
-  splines: {
-    count: 24,
-    radius: 1,
-  },
+const diameter = {
+  shaft: 8,
+  inner: 10,
+  outer: 12,
+};
+const height = {
+  bottom: 10,
+  top: 4,
+};
+const splines = {
+  count: 24,
+  radius: 1,
+  inset: 0.5,
 };
 
 // A function declaration that returns geometry
@@ -24,22 +23,22 @@ export const main = () => {
   return subtract(
     union(
       cylinder({
-        height: ferrule.height.top,
-        radius: ferrule.diameter.outer / 2,
-        center: [0, 0, ferrule.height.top / 2],
+        height: height.top,
+        radius: diameter.outer / 2,
+        center: [0, 0, height.top / 2],
         segments,
       }),
       cylinder({
-        height: ferrule.height.bottom,
-        radius: ferrule.diameter.inner / 2,
-        center: [0, 0, -ferrule.height.bottom / 2],
+        height: height.bottom,
+        radius: (diameter.inner + splines.inset) / 2,
+        center: [0, 0, -height.bottom / 2],
         segments,
       })
     ),
     cylinder({
-      height: ferrule.height.bottom + ferrule.height.top,
-      radius: ferrule.diameter.shaft / 2,
-      center: [0, 0, (ferrule.height.top - ferrule.height.bottom) / 2],
+      height: height.bottom + height.top,
+      radius: diameter.shaft / 2,
+      center: [0, 0, (height.top - height.bottom) / 2],
       segments,
     })
   );

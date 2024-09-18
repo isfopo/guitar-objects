2b745a09aa0f74fd496b27d352a1c600211e5844
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-17 22:14:26 -0400

    📝 (bass-tuner-ferrule.ts): add missing circle primitive import and extrudeRotate function import   ♻️ (bass-tuner-ferrule.ts): refactor main function by splitting the geometry creation into separate functions for top, bottom, and shaft parts to improve readability and maintainability

--- src/bass-tuner-ferrule.ts
+++ src/bass-tuner-ferrule.ts
@@ -1,12 +1,13 @@
 import { subtract, union } from "@jscad/modeling/src/operations/booleans";
-import { cylinder } from "@jscad/modeling/src/primitives";
+import { circle, cylinder } from "@jscad/modeling/src/primitives";
+import { extrudeRotate } from "@jscad/modeling/src/operations/extrusions";
 
 const segments = 50;
 
 const diameter = {
   shaft: 8,
   inner: 10,
-  outer: 12,
+  outer: 16,
 };
 const height = {
   bottom: 10,
@@ -18,28 +19,41 @@ const splines = {
   inset: 0.5,
 };
 
+const top = () => {
+  const bevel = extrudeRotate(
+    { segments },
+    circle({ radius: height.top / 2, center: [diameter.outer / 2, height.top] })
+  );
+
+  return subtract(
+    cylinder({
+      height: height.top,
+      radius: diameter.outer / 2,
+      center: [0, 0, height.top / 2],
+      segments,
+    }),
+    bevel
+  );
+};
+
+const bottom = () => {
+  return cylinder({
+    height: height.bottom,
+    radius: (diameter.inner + splines.inset) / 2,
+    center: [0, 0, -height.bottom / 2],
+    segments,
+  });
+};
+
+const shaft = () =>
+  cylinder({
+    height: height.bottom + height.top,
+    radius: diameter.shaft / 2,
+    center: [0, 0, (height.top - height.bottom) / 2],
+    segments,
+  });
+
 // A function declaration that returns geometry
 export const main = () => {
-  return subtract(
-    union(
-      cylinder({
-        height: height.top,
-        radius: diameter.outer / 2,
-        center: [0, 0, height.top / 2],
-        segments,
-      }),
-      cylinder({
-        height: height.bottom,
-        radius: (diameter.inner + splines.inset) / 2,
-        center: [0, 0, -height.bottom / 2],
-        segments,
-      })
-    ),
-    cylinder({
-      height: height.bottom + height.top,
-      radius: diameter.shaft / 2,
-      center: [0, 0, (height.top - height.bottom) / 2],
-      segments,
-    })
-  );
+  return subtract(union(top(), bottom()), shaft());
 };
